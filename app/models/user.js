b7f29ec444960bd001f0afeebaca66e1d98eadc6
DobryPpz <xaleksx960@gmail.com>
2022-04-21 03:35:29 +0200

    dodanie paru katalogów do frontendu i backendu. model przechowywania użytkownika

--- app/models/user.js
+++ app/models/user.js
@@ -1,2 +1,28 @@
 const mongoose = require("mongoose");
 
+const User = new mongoose.model(
+    "User",
+    new mongoose.Schema({
+        username: String,
+        password: String,
+        level: Number,
+        skincolor: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "SkinColor"
+        },
+        bulletcolor: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "BulletColor"
+        },
+        deathsound: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "DeathSound"
+        },
+        shootsound: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "ShootSound"
+        }
+    })
+);
+
+module.exports = User;
